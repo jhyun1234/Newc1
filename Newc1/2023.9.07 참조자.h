29eb7366f725dbdfe9206a0c33b0120cf77b1553
jhyun1234 <anjyounl24@gmail.com>
2023-09-08 19:46:54 +0900

    2023.9.8 이름공간

---
+++ Newc1/2023.9.07 참조자.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <iostream>
+
+void Damage(int hp)
+{
+	hp -= 100;
+}
+
+int main()
+{
+#pragma region 스트림
+	// 시간의 흐름에 따라 연속적으로 발생하는 
+	// 데이터의 흐름이다.
+
+	char alphabet = 'A';
+	int data = 100;
+
+	// 스트림의 경우 입력된 데이터는 출력 장치로 전달하며,
+	// "<<" 연산자를 사용하여 자신이 참조한 값을 반환한다.
+
+
+	// std::cout << "alphabet의 값 : " << (int)alphabet << std::endl;
+
+	// std::cin >> data;
+
+	// 스트림으로 입력받을때 ">>" 연산자를 사용하여 버퍼에
+	// 저장한 다음 NULL 문자까지만 버퍼 안의 내용을 출력한다.
+
+	// std::cout << "data의 값 : "<< data << std::endl;
+
+	// 스트림은 운영체제에 의해 생성되며,
+	// 스트림 자체에 버퍼라는 임시 메모리 공간이 존재한다.
+
+
+
+#pragma endregion
+
+#pragma region 동적 할당과 해제
+
+	// int* ptr = new int;
+	// 
+	// *ptr = 225;
+	// std::cout << "ptr 가리키는 값 :" << *ptr << std::endl;
+	// 
+	// delete ptr;
+	// 
+	// ptr = nullptr;
+	// 
+	// ptr = new int[5];
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 	ptr[i] = i;
+	// 	std::cout << ptr[i] << std::endl;
+	// }
+	// delete [] ptr;
+
+#pragma endregion
+
+#pragma region 참조자
+
+	// int value = 200;
+	// int& ref = value;
+	// 
+	// Damage(value);
+	// 
+	// std::cout << "value 의 값 :" << value << std::endl;
+	// std::cout << "ref 의 값 :" << ref << std::endl;
+	// 
+	// ref = 999;
+	// std::cout << "value 의 값 :" << value << std::endl;
+	// std::cout << "ref 의 값 :" << ref << std::endl;
+#pragma endregion
+
+#pragma region 범위 기반 for문
+
+	// int dataList[5] = { 1,2,3,4,5 };
+	// 
+	// for (const int & element : dataList)
+	// {
+	// 	std::cout << element << std::endl;
+	// 
+	// }
+#pragma endregion
+
+
+
+	return 0;
+
+}
--- Newc1/Newc1.cpp
+++ Newc1/Newc1.cpp
@@ -1,88 +1,101 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
-void Damage(int hp)
+
+namespace A
 {
-	hp -= 100;
+	void Attack()
+	{
+		cout << "A 개발자 Attack( )" << endl;
+	}
+}
+
+namespace B
+{
+	void Attack()
+	{
+		cout << "B 개발자 Attack( )" << endl;
+	}
+
+}
+
+// using namespace 은 밑에 사용
+void Damage(int hp=100)
+{
+	hp -= 25;
+	cout << "hp의 값 : " << hp << endl;
+}
+
+// 기본 매개변수는 오른쪽에서 부터 정의해야 한다.
+void Move(int x , int y, int z =10) 
+{
+	cout << "x의 값 :" << x << endl;
+	cout << "y의 값 :" << y << endl;
+	cout << "z의 값 :" << z << endl;
 }
 
 int main()
 {
-#pragma region 스트림
-	// 시간의 흐름에 따라 연속적으로 발생하는 
-	// 데이터의 흐름이다.
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는데
+	// 사용하는 연산자이다.
 
-	char alphabet = 'A';
-	int data = 100;
+	// int data = 100;
+	// 
+	// std::cout << "지역 변수data의 값 :" << data << std::endl;
+	// std::cout << "전역 변수data의 값 :" << ::data << std::endl;
+	// 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가
+	// 선언되었을 때 가장 가까운 범위에 선언된 변수의 이름을
+	// 사용하는 범위 규칙이 존재하기 때문에 전역 변수가 호출되지 않는다.
 	
-	// 스트림의 경우 입력된 데이터는 출력 장치로 전달하며,
-	// "<<" 연산자를 사용하여 자신이 참조한 값을 반환한다.
-
 	
-	// std::cout << "alphabet의 값 : " << (int)alphabet << std::endl;
+#pragma endregion
 
-	// std::cin >> data;
-	
-	// 스트림으로 입력받을때 ">>" 연산자를 사용하여 버퍼에
-	// 저장한 다음 NULL 문자까지만 버퍼 안의 내용을 출력한다.
-
-	// std::cout << "data의 값 : "<< data << std::endl;
-
-	// 스트림은 운영체제에 의해 생성되며,
-	// 스트림 자체에 버퍼라는 임시 메모리 공간이 존재한다.
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역이다.
 
 
+	// A::Attack();
+	// B::Attack();
+#pragma endregion
+
+#pragma region 기본 매개변수
+	// 함수의 매개 변수에 값이 전달되지 않았을 때
+	// 기본 값으로 설정되는 매개 변수이다.
+
+	//int hp = 100;
+	//Damage();
+	//Move(10, 20);
+
+	//// 기본 매개변수가 있는 위치에 다시 새로운 인수를
+	//// 넣어줄 수 있다.
+	//
+	//Move(5, 10, 30);
 
 #pragma endregion
 
-#pragma region 동적 할당과 해제
+#pragma region 최소 공배수
+	/*
+	int a = 0;
+	int b = 0;
+	int i = 0;
+	int x = 1; // 최대 공약수
+	cout << "정수를 입력하세요 :" << endl;
+	cin >> a >> b;
 
-	// int* ptr = new int;
-	// 
-	// *ptr = 225;
-	// std::cout << "ptr 가리키는 값 :" << *ptr << std::endl;
-	// 
-	// delete ptr;
-	// 
-	// ptr = nullptr;
-	// 
-	// ptr = new int[5];
-	// 
-	// for (int i = 0; i < 5; i++)
-	// {
-	// 	ptr[i] = i;
-	// 	std::cout << ptr[i] << std::endl;
-	// }
-	// delete [] ptr;
-
+	for (i = 2; i <= a && i <= b; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			x *= i;
+			a /= i;
+			b /= i;
+			i--;
+		}
+	}
+	cout << "최소 공배수 : " << x*a*b << endl;
+	*/
 #pragma endregion
-
-#pragma region 참조자
-
-	// int value = 200;
-	// int& ref = value;
-	// 
-	// Damage(value);
-	// 
-	// std::cout << "value 의 값 :" << value << std::endl;
-	// std::cout << "ref 의 값 :" << ref << std::endl;
-	// 
-	// ref = 999;
-	// std::cout << "value 의 값 :" << value << std::endl;
-	// std::cout << "ref 의 값 :" << ref << std::endl;
-#pragma endregion
-
-#pragma region 범위 기반 for문
-
-	// int dataList[5] = { 1,2,3,4,5 };
-	// 
-	// for (const int & element : dataList)
-	// {
-	// 	std::cout << element << std::endl;
-	// 
-	// }
-#pragma endregion
-
 
 
 	return 0;
