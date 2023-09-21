26179cb1523053039f62b99158b82b1be0d8fb68
KIM <anjyounl24@gmail.com>
2023-09-21 19:47:31 +0900

    2023.9.21 템플릿

---
+++ Newc1/LinkedList.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class LinkedList
+{
+private:
+	T data;
+
+public:
+	LinkedList() = default;
+
+	void Push( T data )
+	{
+		this->data = data;
+
+		cout << "push가 되어진 data의 값 : " << this->data << endl;
+	}
+
+
+
+};
+
+
