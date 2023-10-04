ff33998a9e2cf8c0f322844689e63323cf63214c
jhyun1234 <anjyounl24@gmail.com>
2023-10-04 19:46:00 +0900

    2023.10.04 컨테이너 어댑터

--- Newc1/Newc1.cpp
+++ Newc1/Newc1.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
 #pragma region 컨테이너 어댑터
-{
+
 	// 기존 컨테이너의 인터페이스를 제한하여 만든 기능이
 	// 제한되거나 변형된 컨테이너 이다.
-}
+
    
 
 
@@ -15,7 +17,54 @@ using namespace std;
 
 int main()
 {
+#pragma region  컨테이너 어댑터(stack)
 
+
+
+	//std::stack<int> stack;
+
+	//stack.push( 10 );
+	//stack.push( 20 );
+	//stack.push( 30 );
+	//stack.push( 40 );
+	//stack.push( 50 );
+
+	//// cout << "Stack의 TOP : " << stack.top() << endl;
+
+	//
+	//while ( stack.empty() == false )
+	//{
+	//	cout << "Stack의 TOP : " << stack.top() << endl;
+	//	stack.pop();
+
+	//}
+
+
+	// 50
+	// 40
+	// 30
+	// 20
+	// 10
+
+#pragma endregion
+
+
+#pragma region 컨테이너 어댑터(Queue)
+
+	std::queue<int> queue;
+	queue.push( 10 );
+	queue.push( 20 );
+	queue.push( 30 );
+	queue.push( 40 );
+	queue.push( 50 );
+
+	while ( queue.size() )
+	{
+		cout << queue.front() << endl;
+		queue.pop();
+	}
+
+#pragma endregion
 
 
 	return 0;
